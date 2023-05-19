6eaf546d2c8fab442db462830726014c8b2b8944
McastIII <jeancapunocapuno@gmail.com>
2023-05-19 15:50:22 +0800

    second commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,53 +8,13 @@ toggleMenu.addEventListener("click", () => {
     header.classList.toggle("open");
 });
 
-
-
-// // dropdown
-// const links = document.querySelectorAll(".links__drop__item h4");
-
-// links.forEach((baba) => {
-//     baba.addEventListener("click", () => {
-//     baba.nextElementSibling.classList.toggle("open");
-//     baba.querySelector("i").classList.toggle("open");
-//     });
-// });
-
-// //coffee nav
-// const starNav = document.querySelectorAll(".star__nav__link");
-// const starContent = document.querySelectorAll(".coffee__tab__content");
-// starNav.forEach((star) => {
-//     star.addEventListener("click", () => {
-//     removeActiveStar();
-//     star.classList.add("active");
-//     const activeContent = document.querySelector(`#${star.id}-content`);
-//     removeActiveContent();
-//     activeContent.classList.add("active");
-//     });
-// });
-
-// function removeActiveStar() {
-//     starNav.forEach((star) => {
-//     star.classList.remove("active");
-//     });
-// }
-
-// function removeActiveContent() {
-//     starContent.forEach((star) => {
-//     star.classList.remove("active");
-//     });
-// }
-
 // // header scroll
-// const header = document.querySelector(".header");
-// const brandimg = document.querySelector(".branding img");
-// window.addEventListener("scroll", () => {
-//     console.log(window.pageYOffset);
-//     if (window.pageYOffset >= 100) {
-//     header.classList.add("blue");
-//     brandimg.style = "background-color:red";
-//     } else {
-//     header.classList.remove("blue");}
-
-// });
-
+const burger = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 50) {
+    burger.classList.add("blue");
+  } else {
+    burger.classList.remove("blue");
+  }
+});
